8efe34f6ed0388dc50ca52962332371b9c71be11
suisasa <suisasa@suisasadeMacBook-Air.local>
2026-07-15 13:46:03 +0800

    chore: scaffold Tauri desktop app

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    today_to_do_list_lib::run();
+}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import "./style.css";
+
+document.querySelector<HTMLElement>("#app")!.innerHTML = `
+  <h1>Today To Do List</h1>
+`;
